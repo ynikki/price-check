b82ca3ea3de1c067d3f740eb2c84a276a850440a
Tony Gaskell <thgaskell@gmail.com>
2015-10-24 13:18:42 -1000

    Commit intial frontend code

---
+++ public/js/app.js
@@ -0,0 +1,40 @@
+/**
+ * Returns the device's current location.
+ * @return {object} The device's current location
+ */
+function requestProductsByCurrentPosition () {
+  /* The `getCurrentPosition` takes a function as it's first argument.
+   * This function is referred to as a "callback" function, because it is
+   * called when the result (current location) is found.
+   */
+  navigator.geolocation.getCurrentPosition(/* your function name goes here */);
+}
+
+/**
+ * Sends a request to the server to get Uber products based on passed in
+ * latitude and longitude positions.
+ * @param  {number} lat The location's latitude value
+ * @param  {number} lng The location's longitude value
+ * @return {[Products]]} The Uber products available at the queried location
+ */
+function getProductsByLocation (lat, lng) {
+  var location = {
+    /* location object */
+  };
+  var products = getProducts(location);
+  return products;
+}
+
+/**
+ * Gets the products from a certain location.
+ * @param  {object} The location object to query
+ * @return {[Product]]} An array of products
+ */
+function getProducts (location) {
+  return $.ajax({
+    type: "GET",
+    data: location,
+    url: /* Backend URL */,
+    async: false
+  });
+}
